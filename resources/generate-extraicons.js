072abf309f7b8925b44e06fc917f3ea0932d78db
vdwulp <58190301+vdwulp@users.noreply.github.com>
2025-04-21 21:51:00 +0200

    Update generate-extraicons.js

--- resources/generate-extraicons.js
+++ resources/generate-extraicons.js
@@ -17,12 +17,24 @@ const extraicons = {
     # # # . .
     . . . # .
     . . . . #`,
+  microsoft: `
+    # # . # #
+    # # . # #
+    . . . . .
+    # # . # #
+    # # . # #`,
   microbit: `
     . . . . .
     # # # # #
     # . # . #
     # # # # #
     . . . . .`,
+  music: `
+    . # # # #
+    . # . . #
+    . # . # #
+    # # . # #
+    # # . . .`,
   notes: `
     . . . . #
     . # . . #
@@ -59,12 +71,42 @@ const extraicons = {
     . . # . .
     # . . # .
     . # . . #`,
-  windows: `
-    # # . # #
-    # # . # #
-    . . . . .
-    # # . # #
-    # # . # #`
+  heart: `
+    . # . # .
+    # . # . #
+    # . . . #
+    . # . # .
+    . . # . .`,
+  house: `
+    . . # . .
+    . # # # .
+    # # # # #
+    # . # . #
+    # # # . #`,
+  skull: `
+    . # # # .
+    # # # # #
+    # . # . #
+    . # . # .
+    . # # # .`,
+  duck: `
+    . . # # .
+    . . # # #
+    # # # # .
+    # # # # .
+    . # # . .`,
+  dog: `
+    . . . # #
+    . . . # #
+    # # # # .
+    . # # # .
+    . # . # .`,
+  cat: `
+    # . . # #
+    # . . # #
+    # # # # .
+    . # # # .
+    . # . # .`
 }
 
 // Check/create directories
@@ -92,37 +134,24 @@ function createDirectory( dir ) {
 Object.keys(extraicons).forEach(icon => {
     const data = extraicons[icon];
     const hexLiteral = data
-      .replace(/[ \n`\(\)]/gi, '');
+      .replace( /[ \n`\(\)]/gi, '' );
 
-    const width = 200;
-    const height = 200;
-    var svg = `<svg xmlns="http://www.w3.org/2000/svg" height="${width}" width="${height}">`;
-    for (var i = 0; i < 5; i++) {
-      for (j = 0; j < 5; j++) {
-        const hexItemVal = hexLiteral[(i * 5) + j] ? hexLiteral[(i * 5) + j] == '#' : false;
-        const x = j * (width / 5);
-        const y = i * (height / 5);
-        svg += `<rect y="5" x="5" width="30" height="30" rx="5" transform="translate(${x},${y})"
-                fill="${hexItemVal ? '#fff' : '#000'}"
-                fill-opacity="${hexItemVal ? '1.0' : '0.2'}"/>\n`;
+    const w = 5;
+    const h = 5;
+    const width = w * 40;
+    const height = h * 40;
+    var svg = `<svg xmlns="http://www.w3.org/2000/svg" height="${ width }" width="${ height }">`;
+    for ( var i = 0; i < h; i++ ) {
+      for ( var j = 0; j < w; j++ ) {
+        const hexItemVal = hexLiteral[ ( i * 5 ) + j ] ? hexLiteral[ ( i * 5 ) + j ] == '#' : false;
+        const x = j * 40;
+        const y = i * 40;
+        svg += `<rect y="5" x="5" width="30" height="30" rx="5" transform="translate(${ x },${ y })"
+                fill="${ hexItemVal ? '#fff' : '#000' }"
+                fill-opacity="${ hexItemVal ? '1.0' : '0.2' }"/>\n`;
       }
     }
     svg += `</svg>`;
-
-/*    var svg = `<svg xmlns="http://www.w3.org/2000/svg" height="200" width="200">`;
-    const width = 200;
-    const height = 200;
-    for (var i = 0; i < 5; i++) {
-      for (j = 0; j < 5; j++) {
-        const hexItemVal = hexLiteral[(i * 5) + j] ? hexLiteral[(i * 5) + j] == '#' : false;
-        const x = j * (width / 5);
-        const y = i * (height / 5);
-        svg += `<rect y="5" x="5" width="30" height="30" rx="5" transform="translate(${x},${y})"
-                fill="${hexItemVal ? '#fff' : '#000'}"
-                fill-opacity="${hexItemVal ? '1.0' : '0.2'}"/>\n`;
-      }
-    }
-    svg += `</svg>`;*/
   
     const svgPath = path.join( __dirname, namespace, svgdir, icon + ".svg" );
     console.log( "Creating image " + svgPath );
