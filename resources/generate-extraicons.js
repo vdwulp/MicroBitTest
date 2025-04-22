ac7e41d123a8e40caa6b9072a24e9b134635237b
vdwulp <58190301+vdwulp@users.noreply.github.com>
2025-04-22 15:39:52 +0200

    Update generate-extraicons.js

--- resources/generate-extraicons.js
+++ resources/generate-extraicons.js
@@ -10,109 +10,143 @@ const extraicons = {
     # # # # #
     # # # # #
     # # # # #
-    . # # # .`,
+    . # # # .
+    `,
   magnifier: `
     # # # . .
     # . # . .
     # # # . .
     . . . # .
-    . . . . #`,
+    . . . . #
+    `,
   microsoft: `
     # # . # #
     # # . # #
     . . . . .
     # # . # #
-    # # . # #`,
+    # # . # #
+    `,
   microbit: `
     . . . . .
     # # # # #
     # . # . #
     # # # # #
-    . . . . .`,
+    . . . . .
+    `,
   music: `
     . # # # #
     . # . . #
     . # . # #
     # # . # #
-    # # . . .`,
+    # # . . .
+    `,
   notes: `
     . . . . #
     . # . . #
     . # . # #
     # # . # #
-    # # . . .`,
+    # # . . .
+    `,
   horizontal: `
     # # # # #
     . . . . .
     # # # # #
     . . . . .
-    # # # # #`,
+    # # # # #
+    `,
   vertical: `
     # . # . #
     # . # . #
     # . # . #
     # . # . #
-    # . # . #`,
+    # . # . #
+    `,
   grid: `
     # . # . #
     . . . . .
     # . # . #
     . . . . .
-    # . # . #`,
+    # . # . #
+    `,
   diagonal1: `
     . # . . #
     # . . # .
     . . # . .
     . # . . #
-    # . . # .`,
+    # . . # .
+    `,
   diagonal2: `
     # . . # .
     . # . . #
     . . # . .
     # . . # .
-    . # . . #`,
+    . # . . #
+    `,
   heart: `
     . # . # .
     # . # . #
     # . . . #
     . # . # .
-    . . # . .`,
+    . . # . .
+    `,
   house: `
     . . # . .
     . # # # .
     # # # # #
     # . # . #
-    # # # . #`,
+    # # # . #
+    `,
   skull: `
     . # # # .
     # # # # #
     # . # . #
     . # . # .
-    . # # # .`,
+    . # # # .
+    `,
   duck: `
     . . # # .
     . . # # #
     # # # # .
     # # # # .
-    . # # . .`,
+    . # # . .
+    `,
   dog: `
     . . . # #
     . . . # #
     # # # # .
     . # # # .
-    . # . # .`,
+    . # . # .
+    `,
   cat: `
     # . . # #
     # . . # #
     # # # # .
     . # # # .
-    . # . # .`
+    . # . # .
+    `
 }
 
 // Check/create directories
 console.log("Current directory: ", __dirname);
 createPath( namespace, svgdir )
 
+// Generate SVG files
+Object.keys(extraicons).forEach(icon => {
+    const data = extraicons[icon];
+    const hex = data
+      .replace( /[ \n`\(\)]/gi, '' );
+
+    const svg = iconToSVG( hex, 5, 5 );
+  
+    const svgPath = path.join( __dirname, namespace, svgdir, icon + ".svg" );
+    console.log( "Creating image " + svgPath );
+    fs.writeFile( svgPath, svg, { encoding: 'utf8', flag: 'w' }, function( err, result ) {
+        if (err) console.log( "Error writing to " + svgPath );
+    });
+});
+
+
+// --- FUNCTIONS ---
 
 function createPath( namespace, dir ) {
   createDirectory( namespace );
@@ -131,31 +165,20 @@ function createDirectory( dir ) {
   }
 }
 
-Object.keys(extraicons).forEach(icon => {
-    const data = extraicons[icon];
-    const hexLiteral = data
-      .replace( /[ \n`\(\)]/gi, '' );
-
-    const w = 5;
-    const h = 5;
-    const width = w * 40;
-    const height = h * 40;
-    var svg = `<svg xmlns="http://www.w3.org/2000/svg" height="${ width }" width="${ height }">`;
-    for ( var i = 0; i < h; i++ ) {
-      for ( var j = 0; j < w; j++ ) {
-        const hexItemVal = hexLiteral[ ( i * 5 ) + j ] ? hexLiteral[ ( i * 5 ) + j ] == '#' : false;
-        const x = j * 40;
-        const y = i * 40;
-        svg += `<rect y="5" x="5" width="30" height="30" rx="5" transform="translate(${ x },${ y })"
-                fill="${ hexItemVal ? '#fff' : '#000' }"
-                fill-opacity="${ hexItemVal ? '1.0' : '0.2' }"/>\n`;
-      }
+function iconToSVG( hex, w, h ) {
+  const width = w * 40;
+  const height = h * 40;
+  var svg = `<svg xmlns="http://www.w3.org/2000/svg" height="${ width }" width="${ height }">`;
+  for ( var r = 0; r < h; r++ ) {
+    for ( var c = 0; c < w; c++ ) {
+      const on = hex[ ( r * w ) + c ] ? hex[ ( r * w ) + c ] == '#' : false;
+      const x = c * 40;
+      const y = r * 40;
+      svg += `<rect y="5" x="5" width="30" height="30" rx="5" transform="translate(${ x },${ y })"
+              fill="${ on ? '#fff' : '#000' }"
+              fill-opacity="${ on ? '1.0' : '0.2' }"/>\n`;
     }
-    svg += `</svg>`;
-  
-    const svgPath = path.join( __dirname, namespace, svgdir, icon + ".svg" );
-    console.log( "Creating image " + svgPath );
-    fs.writeFile( svgPath, svg, { encoding: 'utf8', flag: 'w' }, function( err, result ) {
-        if (err) console.log( "Error writing to " + svgPath );
-    });
-});
+  }
+  svg += `</svg>`;
+  return svg; 
+}
